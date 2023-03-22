cf8a10b1b42265195753239ec38f86921382f2ab
FlyingSparrows <tamarakijkindevegte@live.nl>
2023-03-22 14:12:56 +0100

    Added some classes

---
+++ src/com/company/Train.java
@@ -0,0 +1,24 @@
+package com.company;
+
+import java.util.ArrayList;
+
+public class Train {
+    private ArrayList<Coupe> coupes;
+    private ArrayList<Subscriber> subscribers;
+
+    public Train() {
+
+    }
+
+    public void subscribe() {
+
+    }
+
+    public void unsubscribe() {
+
+    }
+
+    public void notifySubscribers() {
+
+    }
+}

55a66449ba78ecd4cb77e0fd1b817fe705b57fdb
FlyingSparrows <tamarakijkindevegte@live.nl>
2023-03-22 14:45:55 +0100

    Added setters and getters to Ticket class

--- src/com/company/Passenger.java
+++ src/com/company/Passenger.java
@@ -5,8 +5,8 @@ import java.util.HashMap;
 
 abstract class Passenger {
     private ArrayList<Emotion> emotions;
-//    private Ticket ticket;
-    private HashMap<EmotionType, float> emotionModifiers;
+    private Ticket ticket;
+//    private HashMap<EmotionType, float> emotionModifiers;
     public Passenger() {
 
     }
